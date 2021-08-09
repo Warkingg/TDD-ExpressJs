a04618f7c4343df784fe2e097d91625303c82d8d
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-08-10 01:53:22 +0700

    Changing Page

--- source/__test__/UserListing.spec.js
+++ source/__test__/UserListing.spec.js
@@ -2,7 +2,6 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
-const { report } = require('../src/user/UserRouter');
 
 beforeAll(async () => {
   await sequelize.sync();
@@ -58,5 +57,16 @@ describe('Listing User', () => {
     await addUsers(15, 7);
     const response = await getUsers();
     expect(response.body.totalPages).toBe(2);
+  });
+  it('returns second page users and page indicator when page is set as 1 request parameter', async () => {
+    await addUsers(11);
+    const response = await getUsers().query({ page: 1 });
+    expect(response.body.content[0].username).toBe('user11');
+    expect(response.body.page).toBe(1);
+  });
+  it('returns first page when page is set below zero as request parameter', async () => {
+    await addUsers(11);
+    const response = await getUsers().query({ page: -5 });
+    expect(response.body.page).toBe(0);
   });
 });
