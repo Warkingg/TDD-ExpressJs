d96eb6eaf4eb26d858f2c8fdf7ac79acdae7b7a6
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-08-09 23:53:44 +0700

    Error Body

--- source/__test__/UserRegister.spec.js
+++ source/__test__/UserRegister.spec.js
@@ -208,6 +208,14 @@ describe('User Registration', () => {
     const users = await User.findAll();
     expect(users.length).toBe(0);
   });
+  it('returns Validation Failure message in error response body when validation fails', async () => {
+    const response = await postUser({
+      username: null,
+      email: validUser.email,
+      password: 'P4ssword',
+    });
+    expect(response.body.message).toBe('Validation Failure');
+  });
 });
 
 describe('Internationalization', () => {
@@ -221,6 +229,7 @@ describe('Internationalization', () => {
   const email_isalreadyregisted = 'E-mail đã được đăng ký';
   const user_create_success = 'Đăng ký thành công';
   const email_failure = 'Gửi E-mail không thành công';
+  const validation_failure = 'Xác thực thất bại';
 
   it.each`
     field         | value              | expectedMessage
@@ -268,6 +277,17 @@ describe('Internationalization', () => {
     simulateSmtpFailure = true;
     const response = await postUser({ ...validUser }, { language: 'vi' });
     expect(response.body.message).toBe(email_failure);
+  });
+  it(`returns ${validation_failure} message in error response body when validation fails`, async () => {
+    const response = await postUser(
+      {
+        username: null,
+        email: validUser.email,
+        password: 'P4ssword',
+      },
+      { language: 'vi' }
+    );
+    expect(response.body.message).toBe(validation_failure);
   });
 });
 
@@ -337,4 +357,38 @@ describe('Account activation', () => {
       expect(response.body.message).toBe(message);
     }
   );
+});
+describe('Error Model', () => {
+  it('returns path, timestamp, message and validationErrors in response when validation failure', async () => {
+    const response = await postUser({ ...validUser, username: null });
+    const body = response.body;
+    expect(Object.keys(body)).toEqual(['path', 'timestamp', 'message', 'validationErrors']);
+  });
+  it('returns path, timestamp, message in response when request fails other than validation error', async () => {
+    const token = 'this-token-does-not-exist';
+    const response = await request(app)
+      .post('/api/1.0/users/token/' + token)
+      .send();
+    const body = response.body;
+    expect(Object.keys(body)).toEqual(['path', 'timestamp', 'message']);
+  });
+  it('returns path in error body', async () => {
+    const token = 'this-token-does-not-exist';
+    const response = await request(app)
+      .post('/api/1.0/users/token/' + token)
+      .send();
+    const body = response.body;
+    expect(body.path).toEqual('/api/1.0/users/token/' + token);
+  });
+  it('returns timestamp in milliseconds within 5 seconds value in error body', async () => {
+    const nowInMillis = new Date().getTime();
+    const fiveSecondsLater = nowInMillis + 5 * 1000;
+    const token = 'this-token-does-not-exist';
+    const response = await request(app)
+      .post('/api/1.0/users/token/' + token)
+      .send();
+    const body = response.body;
+    expect(body.timestamp).toBeGreaterThan(nowInMillis);
+    expect(body.timestamp).toBeLessThan(fiveSecondsLater);
+  });
 });
