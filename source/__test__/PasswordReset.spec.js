02df53ae2f816e1bc96e2934ee4d9700cf65b24b
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-08-11 17:00:23 +0700

    Refactoring Rest  Endpoint

--- source/__test__/PasswordReset.spec.js
+++ source/__test__/PasswordReset.spec.js
@@ -53,7 +53,7 @@ const addUser = async (user = { ...activeUser }) => {
 };
 
 const postPasswordReset = (email = 'user1@mail.com', options = {}) => {
-  const agent = request(app).post('/api/1.0/password-reset');
+  const agent = request(app).post('/api/1.0/password');
   if (options.language) {
     agent.set('Accept-Language', options.language);
   }
@@ -75,7 +75,7 @@ describe('Password Reset Request', () => {
     async ({ language, message }) => {
       const nowInMillis = new Date().getTime();
       const response = await postPasswordReset('user1@mail.com', { language });
-      expect(response.body.path).toBe('/api/1.0/password-reset');
+      expect(response.body.path).toBe('/api/1.0/password');
       expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
       expect(response.body.message).toBe(message);
     }
