e00f0fdfd44f6183a60063405756080205176084
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-08-16 00:34:49 +0700

    Refactoring Configuration

---
+++ source/database/config.js
@@ -0,0 +1,28 @@
+const profiles = require('../config');
+
+const dbConfigs = {};
+
+Object.keys(profiles).forEach((profile) => {
+  dbConfigs[profile] = { ...profiles[profile].database };
+});
+console.log(dbConfigs);
+module.exports = dbConfigs;
+
+// module.exports = {
+//   development: {
+//     username: 'my-db-user',
+//     password: 'db-p4ss',
+//     database: 'hoaxify',
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     storage: './database.sqlite',
+//   },
+//   staging: {
+//     username: 'my-db-user',
+//     password: 'db-p4ss',
+//     database: 'hoaxify',
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     storage: './staging.sqlite',
+//   },
+// };
